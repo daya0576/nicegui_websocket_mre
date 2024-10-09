3c5627819ec2111892a3ddeeae422ff06aabc698
daya0576 <daya0576@gmail.com>
2024-10-10 04:24:03 +0800

    remove start.sh script

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import logging
 
-from fastapi import FastAPI
 from nicegui import app, ui
 
 logging.basicConfig(level=logging.DEBUG)
@@ -13,17 +12,8 @@ async def demo():
     logging.debug("Enable pwa mode")
     app.add_static_files("/images", "images")
 
-    ui.add_head_html(
-        '<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">'
-    )
     ui.add_head_html('<meta name="apple-mobile-web-app-title" content="Beaver">')
     ui.add_head_html('<meta name="apple-mobile-web-app-capable" content="yes">')
-    ui.add_head_html(
-        '<meta name="apple-mobile-web-app-status-bar-style" content="black">'
-    )
-    ui.add_head_html('<meta name="theme-color" content="#121212">')
-
-    # viewBox="90 90 220 220"
     ui.add_head_html(
         '<link rel="apple-touch-icon" href="/images/apple-touch-icon-v4.png">'
     )
@@ -31,5 +21,4 @@ async def demo():
     ui.label("Hello NiceGUI!")
 
 
-fastapi_app = FastAPI()
-ui.run_with(fastapi_app)
+ui.run(port=5001, uvicorn_logging_level="info")
--- start.sh
+++
@@ -1,1 +0,0 @@
-uvicorn main:app --workers 1 --log-level debug --port 8080 --host 0.0.0.0
